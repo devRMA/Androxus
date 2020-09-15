4bb02977fab8bcc04eb370d40851a854c0742a57
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-15 15:56:01 -0300

    Mais informações na mensagem

--- discord/cmds/Ping.py
+++ discord/cmds/Ping.py
@@ -35,7 +35,16 @@ class Ping(commands.Cog):
 
     @commands.command(aliases=['latency', 'latência'], description='Mostra a minha latência atual.')
     async def ping(self, ctx):
-        await ctx.send(f'Minha latência atual é de {int(self.bot.latency * 1000)}ms !')
+        from stopwatch import Stopwatch
+        stopwatch_mensagem = Stopwatch()
+        mensagem_do_bot = await ctx.send(f'Minha latência atual é de {int(self.bot.latency * 1000)}ms !')
+        stopwatch_mensagem.stop()
+        stopwatch_banco = Stopwatch()
+        pegar_o_prefixo(None, ctx) # abre uma conexão, faz um select no banco, e fecha
+        stopwatch_banco.stop()
+        await mensagem_do_bot.edit(content=f'Latência da API do discord: {int(self.bot.latency * 1000)}ms!\n' +
+                                           f'Latência com o banco de dados: {str(stopwatch_banco)}!\n' +
+                                           f'Latência para enviar a mensagem: {str(stopwatch_mensagem)}!')
 
 
 def setup(bot):
