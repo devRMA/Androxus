35589c18f05b4928073250aad322531a37a4d445
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-17 19:02:38 -0300

    Foi adicionado a verificação para saber se o comando foi desativado

--- discord/modelos/EmbedHelp.py
+++ discord/modelos/EmbedHelp.py
@@ -8,6 +8,7 @@ import discord
 from discord.ext import commands
 from datetime import datetime
 from discord.Utils import random_color, pegar_o_prefixo, get_emoji_dance
+from discord.dao.ComandoDesativadoDao import ComandoDesativadoDao
 
 
 def embedHelp(bot: commands.Bot = None,
@@ -22,6 +23,17 @@ def embedHelp(bot: commands.Bot = None,
     prefixo = pegar_o_prefixo(None, ctx)
     exemplo = '\n'.join(exemplos).replace('{pref}', f'{prefixo}')
     como_usar = f'``{prefixo}{comando}`` '
+    comando_esta_desativado = False
+    if ctx.guild is not None:  # se a mensagem foi enviar de um server
+        for comandos_desativados in ComandoDesativadoDao().get_comandos(ctx.guild.id): # for em todos os comandos desativados
+            if comando_esta_desativado: break  # se, dentro do outro for, achar o comando, vai para o for de fora também
+            if comando in comandos_desativados:  # vê se o comando principal, está desativado
+                comando_esta_desativado = True
+                break
+            for comando_alias in aliases:  # vai verificar se algum "sinônimo" desse comando, foi desativado
+                if comando_esta_desativado: break
+                if (comando_alias in comandos_desativados) :  # verifica se o comando está desativado
+                    comando_esta_desativado = True
     if parametros:  # se tiver pelo menos 1 item nos parâmetros
         for c in range(0, len(parametros)):  # vai adicionar `` antes, e depois dos parâmetros, em todos os itens
             parametros[c] = f'``{parametros[c]}``'
@@ -38,9 +50,9 @@ def embedHelp(bot: commands.Bot = None,
                           description=descricao,
                           timestamp=datetime.utcfromtimestamp(datetime.now().timestamp()))
     embed.set_author(name='Androxus',
-                     icon_url=f'{bot.user.avatar_url}')
+                     icon_url=bot.user.avatar_url)
     embed.set_footer(text=f'{ctx.author}',
-                     icon_url=f'{ctx.author.avatar_url}')
+                     icon_url=ctx.author.avatar_url)
     embed.add_field(name='**Como usar?**',
                     value=como_usar,
                     inline=False)
@@ -62,5 +74,9 @@ def embedHelp(bot: commands.Bot = None,
             requisito = f'\nEu preciso ter permissão de ``{perm_pessoa}`` para realizar este comando!'
         embed.add_field(name='<a:atencao:755844029333110815> Requisitos:',
                         value=requisito,
+                        inline=False)
+    if comando_esta_desativado:  # se o comando estiver desativado
+        embed.add_field(name="<a:atencao:755844029333110815> **O comando foi desativado por algum administrador do server!**",
+                        value="**Se você usar este comando, eu não irei responder!**",
                         inline=False)
     return embed
