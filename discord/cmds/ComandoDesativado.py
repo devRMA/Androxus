31ba37fe76caacdc84959eda4cace5317b144245
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-17 14:13:05 -0300

    Primeiro teste, usando o modelo de embed

--- discord/cmds/ComandoDesativado.py
+++ discord/cmds/ComandoDesativado.py
@@ -7,8 +7,8 @@ __author__ = 'Rafael'
 from datetime import datetime
 from discord.ext import commands
 import discord
-from discord.Utils import random_color, pegar_o_prefixo, get_emoji_dance
 from discord.dao.ComandoDesativadoDao import ComandoDesativadoDao
+from discord.modelos.EmbedHelp import EmbedHelp
 
 
 class ComandoDesativado(commands.Cog):
@@ -17,32 +17,21 @@ class ComandoDesativado(commands.Cog):
 
     @commands.command(hidden=True, aliases=['help_disable_command', 'help_dc'])
     async def help_desativar_comando(self, ctx):
-        prefixo = pegar_o_prefixo(None, ctx)
-        embed = discord.Embed(title=f"``{prefixo}desativar_comando``", colour=discord.Colour(random_color()),
-                              description="Desativa um comando!",
-                              timestamp=datetime.utcfromtimestamp(datetime.now().timestamp()))
-        embed.set_author(name="Androxus", icon_url=f"{self.bot.user.avatar_url}")
-        embed.set_footer(text=f"{ctx.author}", icon_url=f"{ctx.author.avatar_url}")
-        embed.add_field(name="**Como usar?**",
-                        value=f"``{prefixo}desativar_comando`` ``\"<comando>\"``",
-                        inline=False)
-        embed.add_field(
-            name="Tudo que estiver entre **<>** são obrigatorio, e tudo que estiver entre **[]** são opcionais.",
-            value=get_emoji_dance(), inline=False)
-        embed.add_field(name="Exemplo:",
-                        value=f"``{prefixo}desativar_comando`` ``say``\n(Esse comando desativa o comando \"say\" no seu servidor!)",
-                        inline=False)
-        embed.add_field(name=":twisted_rightwards_arrows: Sinônimos:",
-                        value=f"``{prefixo}disable_command``, ``{prefixo}dc``", inline=False)
-        embed.add_field(name="<a:atencao:755844029333110815> Requisitos:",
-                        value="Você precisa ter permissão de administrador para usar esse comando!", inline=False)
+        embed = EmbedHelp(self.bot,
+                          ctx,
+                          comando='desativar_comando',
+                          descricao='Desativa comandos!',
+                          parametros=['<"comando">'],
+                          exemplos=['{pref}desativar_comando ``"say"``'],
+                          aliases=self.desativar_comando.aliases,
+                          perm_pessoa=['administrador'])
         await ctx.send(content=ctx.author.mention, embed=embed)
 
     @commands.command(aliases=['disable_command', 'dc'], description='Desativa comandos!')
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
     async def desativar_comando(self, ctx, comando=None):
-        #if not ctx.author.permissions_in(ctx.message.channel).administrator:  # se o usuário não tiver permissão de adm
+        if not ctx.author.permissions_in(ctx.message.channel).administrator:  # se o usuário não tiver permissão de adm
         if comando is None:
             await self.help_desativar_comando(ctx)
             return
