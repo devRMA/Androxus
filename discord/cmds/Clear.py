a3f01f704baa72ca7b4af6f77712fcd9f27abf1c
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-17 08:17:14 -0300

    teste de clear para o wolf

--- discord/cmds/Clear.py
+++ discord/cmds/Clear.py
@@ -33,7 +33,16 @@ class Clear(commands.Cog):
             quantia = int(quantia)  # aqui vai verificar se a pessoa passou um número inteiro
             # se não deu erro, pode continuar
             if quantia <= 300: # um limite, para que a pessoa não consiga pagara 99999 mensagens
-                await ctx.channel.purge(limit=quantia+1)
+                try:
+                    await ctx.channel.purge(limit=quantia+1)
+                except discord.Forbidden:
+                    await ctx.send('O bot não tem permissão para apagar mensagens!')
+                except discord.HTTPException:
+                    await ctx.send('Ocorreu um erro da API na hora de apagar as mensagens!')
+                except Exception as error:
+                    await ctx.send(f'Ocorreu o erro ```{error}```')
+            else:
+                await ctx.send('O limite de mensagens para apagar é 300!')
         except ValueError:
             await ctx.send(embed=embed)
 
