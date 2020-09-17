e62429f087c1ee12c1ea45c05412a0a71772ff08
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-17 14:35:14 -0300

    "__" removido do nome das variaveis

--- discord/modelos/EmbedHelp.py
+++ discord/modelos/EmbedHelp.py
@@ -19,22 +19,22 @@ def embedHelp(bot: commands.Bot = None,
               aliases: list = [],
               perm_pessoa: str = None,
               perm_bot: str = None):
-    __prefixo = pegar_o_prefixo(None, ctx)
-    __exemplos = '\n'.join(exemplos).replace('{pref}', f'{__prefixo}')
-    __aliases = ''
-    __como_usar = f'``{__prefixo}{comando}`` '
+    prefixo = pegar_o_prefixo(None, ctx)
+    exemplos = '\n'.join(exemplos).replace('{pref}', f'{prefixo}')
+    aliases = ''
+    como_usar = f'``{prefixo}{comando}`` '
     if parametros:  # se tiver pelo menos 1 item nos parâmetros
         for c in range(0, len(parametros)):  # vai adicionar `` antes, e depois dos parâmetros, em todos os itens
             parametros[c] = f'``{parametros[c]}``'
-        __como_usar += ' '.join(parametros) # adiciona os parâmetros no "como_usar"
+        como_usar += ' '.join(parametros) # adiciona os parâmetros no "como_usar"
     if aliases:
         for c in range(0, len(aliases)):  # vai adicionar `` antes, e depois de alias
-            aliases[c] = f'``{__prefixo}{aliases[c]}``'
+            aliases[c] = f'``{prefixo}{aliases[c]}``'
         if len(aliases) == 1:
-            __aliases = aliases[0]
+            aliases = aliases[0]
         else:
-            __aliases = ', '.join(aliases)
-    embed = discord.Embed(title=f'``{__prefixo}{comando}``',
+            aliases = ', '.join(aliases)
+    embed = discord.Embed(title=f'``{prefixo}{comando}``',
                           colour=discord.Colour(random_color()),
                           description=descricao,
                           timestamp=datetime.utcfromtimestamp(datetime.now().timestamp()))
@@ -43,17 +43,17 @@ def embedHelp(bot: commands.Bot = None,
     embed.set_footer(text=f'{ctx.author}',
                      icon_url=f'{ctx.author.avatar_url}')
     embed.add_field(name='**Como usar?**',
-                    value=__como_usar,
+                    value=como_usar,
                     inline=False)
     if parametros:  # novamente, só vai entrar, se tiver pelo menos 1 item nos parâmetros
         embed.add_field(
             name='Tudo que estiver entre **<>** são obrigatorio, e tudo que estiver entre **[]** são opcionais.',
             value=get_emoji_dance(), inline=False)
     embed.add_field(name='Exemplo:',
-                    value=__exemplos,
+                    value=exemplos,
                     inline=False)
     embed.add_field(name=':twisted_rightwards_arrows: Sinônimos:',
-                    value=__aliases,
+                    value=aliases,
                     inline=False)
     if perm_pessoa or perm_bot:
         __requisito = ''
