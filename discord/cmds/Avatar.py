3d57f8251e2eb735f51a7bf409137deec071d02d
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-18 18:56:14 -0300

    Pequenas mudanças

--- discord/cmds/Avatar.py
+++ discord/cmds/Avatar.py
@@ -25,19 +25,21 @@ class Avatar(commands.Cog):
     @commands.command(description='Eu vou mandar a foto de perfil da pessoa que você marcar.')
     async def avatar(self, ctx, *args):
         if ctx.message.mentions:  # se tiver alguma menção na mensagem
-            await ctx.send(f'{ctx.message.mentions[0].avatar_url}')  # vai pegar a primeira menção, e pega o avatar da pessoa
+            await ctx.send(
+                f'{ctx.message.mentions[0].avatar_url}')  # vai pegar a primeira menção, e pega o avatar da pessoa
         else:  # se a pessoa não mencionou ninguém, entra aqui
             if args:  # se a pessoa passou pelo menos alguma coisa
                 if len(args) == 1:  # se a pessoa passou mais de um item
                     try:  # vai tentar converter o argumento para int
                         id_de_quem_ver_o_avatar = int(args[0])  # conversão
-                        user = self.bot.get_user(id_de_quem_ver_o_avatar)  # se chegou aqui, vai tentar pegar o usuário com esse id
+                        user = self.bot.get_user(
+                            id_de_quem_ver_o_avatar)  # se chegou aqui, vai tentar pegar o usuário com esse id
                         if user is not None:  # se achou uma pessoa
                             await ctx.send(f'{user.avatar_url}')  # vai mandar o avatar desta pessoa
                         else:  # se o user for None, é porque o bot não achou esse usuário
                             await ctx.send('<a:sad:755774681008832623> Não consegui encontrar o usuário' +
                                            f' <@{id_de_quem_ver_o_avatar}>\nEu preciso ter pelo menos 1' +
-                                           ' servidor em comum com a pessoa, para conseguir encontrar ele.')
+                                           ' servidor em comum com a pessoa, para conseguir encontrar ela.')
                     except ValueError:  # se der erro, é porque a pessoa não passou apenas números
                         await ctx.send(f'<a:atencao:755844029333110815> O valor ``{args[0]}`` não é um id valido!')
                 else:  # se a pessoa passou mais de 1 argumento
