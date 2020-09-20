f4eefab3462bcdc871654d44b3f6d7ca910ce9cb
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-19 23:50:54 -0300

    Agora o comando info mostra a idade do bot

--- discord/cmds/Botinfo.py
+++ discord/cmds/Botinfo.py
@@ -13,6 +13,7 @@ from stopwatch import Stopwatch
 import psutil
 from sys import version
 from discord.dao.InformacoesDao import InformacoesDao
+from dateutil.relativedelta import relativedelta  # módulo que vai ser usado para subtrair datetime
 
 
 class Botinfo(commands.Cog):
@@ -39,12 +40,40 @@ class Botinfo(commands.Cog):
                               colour=discord.Colour(random_color()),
                               description=f'Caso você queira saber meus outros comandos, use ``{prefixo}help``!',
                               timestamp=datetime.utcnow())
-        embed.set_author(name='Androxus', icon_url=self.bot.user.avatar_url)
-        embed.set_footer(text=f'{ctx.author}', icon_url=ctx.author.avatar_url)
-        embed.add_field(name=':calendar: Criado em:', value=f'``{self.bot.user.created_at.strftime("%d/%m/%Y")}``', inline=True)
-        embed.add_field(name=':robot: Meu perfil:', value=f'``{str(self.bot.user)}``', inline=True)
-        embed.add_field(name=':id: Meu id:', value=f'``{self.bot.user.id}``', inline=True)
-        embed.add_field(name='<:WumpusCrown:756712226978660392> Meu dono:', value=f'``{self.bot.get_user(self.bot.owner_id)}``', inline=True)
+        embed.set_author(name='Androxus',
+                         icon_url=self.bot.user.avatar_url)
+        embed.set_footer(text=f'{ctx.author}',
+                         icon_url=ctx.author.avatar_url)
+        embed.add_field(name=':calendar: Data que fui criado:',
+                        value=f'``{self.bot.user.created_at.strftime("%d/%m/%Y")}``',
+                        inline=True)
+        # years months days hours minutes seconds
+        idade = relativedelta(datetime.utcnow(), self.bot.created_at)
+        days = idade.days
+        months = idade.months
+        years = idade.years
+        meses_formatado = ''
+        dias_formatados = ''
+        if days > 1:
+            dias_formatados = f'e {days} dias!'
+        elif days == 1:
+            dias_formatados = f'e {days} dia!'
+        if months > 1:
+            meses_formatado = f', {months} meses '
+        elif months == 1:
+            meses_formatado = f', {months} mês '
+        embed.add_field(name=':older_adult: Idade:',
+                        value=f'``{years} anos {meses_formatado}{dias_formatados}``',
+                        inline=True)
+        embed.add_field(name=':robot: Meu perfil:',
+                        value=f'``{str(self.bot.user)}``',
+                        inline=True)
+        embed.add_field(name=':id: Meu id:',
+                        value=f'``{self.bot.user.id}``',
+                        inline=True)
+        embed.add_field(name='<:WumpusCrown:756712226978660392> Meu dono:',
+                        value=f'``{self.bot.get_user(self.bot.owner_id)}``',
+                        inline=True)
         embed.add_field(name='<a:pato:755774683348992060> Quantos servidores estão me usando:',
                         value=f'``{len(self.bot.guilds)}``',
                         inline=True)
@@ -61,10 +90,11 @@ class Botinfo(commands.Cog):
                         value=f'``{(psutil.virtual_memory().total / (1e+9)):.2f}Gbs' +
                               f'(usando {psutil.virtual_memory().percent}%)``',
                         inline=True)
-        embed.add_field(name='<:WumpusPizza:756712226710356122> Versão da API do discord:',
+        embed.add_field(name='<:WumpusPizza:756712226710356122> Versão do [discord.py]' +
+                             '(https://discordpy.readthedocs.io/en/latest/index.html):',
                         value=f'``{discord.__version__}``',
                         inline=True)
-        embed.add_field(name='<:python:756712226210971660> Versão do python:',
+        embed.add_field(name='<:python:756712226210971660> Versão do [python](https://www.python.org/):',
                         value=f'``{version[0:5]}``',
                         inline=True)
         embed.add_field(name=':bank: Banco de dados que estou usando:',
