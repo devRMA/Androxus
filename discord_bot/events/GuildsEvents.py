5158b235f602db6b4cd0fe5387ea95c62f1275d3
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-23 20:27:05 -0300

    Agora, assim que o bot entra num servidor, ele envia uma mensagem de ajuda!

--- discord_bot/events/GuildsEvents.py
+++ discord_bot/events/GuildsEvents.py
@@ -1,14 +1,17 @@
 # coding=utf-8
 # Androxus bot
-# ErrorCommands.py
+# GuildsEvents.py
 
 __author__ = 'Rafael'
 
 from discord.ext import commands
+import discord
+
 from discord_bot.dao.ServidorDao import ServidorDao
+from discord_bot.utils.Utils import get_emoji_dance, pegar_o_prefixo
 
 
-class ErrorCommands(commands.Cog):
+class GuildsEvents(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -16,6 +19,14 @@ class ErrorCommands(commands.Cog):
     async def on_guild_join(self, ctx, guild):
         # toda vez que adicionarem o bot num servidor, vai adicionar o servidor ao banco
         ServidorDao().create(guild.id)
+        try:
+            for channel in sorted(guild.channels):
+                if channel.permissions_for(guild.me).send_messages and isinstance(channel, discord.TextChannel):
+                    await channel.send(f'{get_emoji_dance()}\nOlá! Obrigado por me adicionar em seu servidor!\n' +
+                                       f'Para saber meus comandos, digite ``{pegar_o_prefixo(None, ctx)}help``!')
+                    return
+        except:
+            pass
 
     @commands.Cog.listener()
     async def on_guild_remove(self, ctx, guild):
@@ -24,4 +35,4 @@ class ErrorCommands(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(ErrorCommands(bot))
+    bot.add_cog(GuildsEvents(bot))
