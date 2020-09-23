9394979c41215316d3cd1ef9ee16e5a232ecf01b
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-22 21:38:33 -0300

    Alguns checks removidos

--- discord_bot/utils/permissions.py
+++ discord_bot/utils/permissions.py
@@ -26,18 +26,3 @@ def has_permissions(*, check=all, **perms):
 
     return commands.check(pred)
 
-
-def can_send(ctx):
-    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).send_messages
-
-
-def can_embed(ctx):
-    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).embed_links
-
-
-def can_upload(ctx):
-    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).attach_files
-
-
-def can_react(ctx):
-    return isinstance(ctx.channel, discord.DMChannel) or ctx.channel.permissions_for(ctx.guild.me).add_reactions
