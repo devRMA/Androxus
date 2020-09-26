6db5298a082e0931df2e63181194cf85bc2a5110
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-25 22:10:43 -0300

    Tratamento de erros desativado, para testes

--- discord_bot/events/ErrorCommands.py
+++ discord_bot/events/ErrorCommands.py
@@ -12,7 +12,7 @@ class ErrorCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
+    """@commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if self.bot.tratar_erros:
             # font: https://gist.github.com/EvieePy/7822af90858ef65012ea500bcecf1612
@@ -65,7 +65,7 @@ class ErrorCommands(commands.Cog):
                     except:
                         print(f'Ocorreu o erro: {error}\nNa execução do comando {ctx.message.content}')
         else:
-            print(f'Ocorreu o erro: {error}\nNa execução do comando {ctx.message.content}')
+            print(f'Ocorreu o erro: {error}\nNa execução do comando {ctx.message.content}')"""
 
 
 def setup(bot):
