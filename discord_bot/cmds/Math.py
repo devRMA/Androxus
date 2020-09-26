18b3029687f076786f8271f793c45e0eb43daa9e
devRMA <61068108+devRMA@users.noreply.github.com>
2020-09-26 16:29:09 -0300

    Adição da regra de três inversamente proporcional.

--- discord_bot/cmds/Math.py
+++ discord_bot/cmds/Math.py
@@ -187,7 +187,7 @@ class Math(commands.Cog):
         elif 'diretamente'.startswith(msg.content.lower()):
             modo = 'd'
         if modo:
-            await ctx.send('Modo selecionado: ``diretamente proporcional``!')
+            await ctx.send('Modo selecionado: ``inversamente proporcional``!')
             if exists('discord_bot/'):
                 path = 'discord_bot/'
             else:
@@ -270,7 +270,82 @@ class Math(commands.Cog):
                 await ctx.send(embed=embed,
                                file=discord.File(f'{path}images/regra_de_tres_direta-edited.png'))
             elif modo == 'i':
-                pass
+                valores = [
+                    ['primeiro', 'v1'],
+                    ['segundo', 'v2'],
+                    ['terceiro', 'v3']
+                ]
+                pos_text_list = [
+                    [(60, 280), 'v1'],
+                    [(400, 280), 'v2'],
+                    [(60, 370), 'v3'],
+                    [(400, 370), 'x']
+                ]
+                valores_user = []
+                for valor in valores:
+                    img = Image.open(f'{path}images/regra_de_tres_inversa.png')
+                    draw = ImageDraw.Draw(img)
+                    font = ImageFont.truetype(f'{path}fonts/helvetica-normal.ttf', 25)
+                    black = (0, 0, 0)  # rgb
+                    red = (255, 0, 0)  # rgb
+                    for pos_text in pos_text_list:
+                        if pos_text[-1] == valor[-1]:
+                            draw.text(pos_text[0], pos_text[-1], red, font=font)
+                        else:
+                            draw.text(pos_text[0], pos_text[-1], black, font=font)
+                    img.save(f'{path}images/regra_de_tres_inversa-edited.png')
+                    img.close()
+                    await ctx.send(f'Agora, eu preciso que você me fale o {valor[0]} valor (**{valor[-1]} na foto**).',
+                                   file=discord.File(f'{path}images/regra_de_tres_inversa-edited.png'))
+                    try:
+                        value = await self.bot.wait_for('message', check=check, timeout=30)
+                        try:
+                            value = int(value.content)
+                        except ValueError:
+                            value = float(value.content.replace(',', '.'))
+                        except:
+                            return await ctx.send(f'O valor ``{value.content}`` não é um número!')
+                    except asyncio.TimeoutError:
+                        return await ctx.send('Tempo esgotado!')
+                    for c in range(0, len(pos_text_list)):
+                        if pos_text_list[c][-1] == valor[-1]:
+                            pos_text_list[c][-1] = f'{value}'
+                    valores_user.append(value)
+                mult = valores_user[0] * valores_user[1]
+                resp = mult / valores_user[-1]
+                embed = discord.Embed(title=f'Regra de 3!',
+                                      colour=discord.Colour(random_color()),
+                                      description='Passo a passo da resolução:\n' +
+                                                  f'**{valores_user[-1]}x = {valores_user[0]}×{valores_user[1]}**\n' +
+                                                  f'**{valores_user[-1]}x = {mult}**\n' +
+                                                  f'**x = {mult}/{valores_user[-1]}**\n' +
+                                                  f'**x = {resp}**',
+                                      timestamp=datetime.utcnow())
+                embed.set_author(name='Androxus',
+                                 icon_url=self.bot.user.avatar_url)
+                embed.set_footer(text=f'{ctx.author}',
+                                 icon_url=ctx.author.avatar_url)
+                img = Image.open(f'{path}images/regra_de_tres_inversa.png')
+                draw = ImageDraw.Draw(img)
+                font = ImageFont.truetype(f'{path}fonts/helvetica-normal.ttf', 25)
+                black = (0, 0, 0)  # rgb
+                red = (255, 0, 0)  # rgb
+                pos1 = (60, 280)  # x e y
+                pos2 = (400, 280)  # x e y
+                pos3 = (60, 370)  # x e y
+                posx = (400, 370)  # x e y
+                text1 = str(valores_user[0])
+                text2 = str(valores_user[1])
+                text3 = str(valores_user[2])
+                x = str(resp)
+                draw.text(pos1, text1, black, font=font)
+                draw.text(pos2, text2, black, font=font)
+                draw.text(pos3, text3, black, font=font)
+                draw.text(posx, x, red, font=font)
+                img.save(f'{path}images/regra_de_tres_inversa-edited.png')
+                img.close()
+                await ctx.send(embed=embed,
+                               file=discord.File(f'{path}images/regra_de_tres_inversa-edited.png'))
         else:
             await ctx.send(f'{ctx.author.mention} eu não sei o que é ' +
                            f'``{msg.content}``! Eu aceito apenas ``inversamente`` ou ``diretamente``')
