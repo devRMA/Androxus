79892685061a4544c82004bba860bd1aca4db84f
Rafael <rafael271alves@gmail.com>
2021-09-25 14:50:05 -0300

    renomeação do arquivo

--- bot/utils/console.py
+++ bot/utils/console.py
@@ -1,6 +1,6 @@
 from colorama import Fore, Style
 
 
-def log(tag, text, first_color=Fore.GREEN, second_color=Fore.LIGHTMAGENTA_EX):
+def log(tag, text, first_color=Fore.CYAN, second_color=Fore.LIGHTBLUE_EX):
     print(f'{Style.BRIGHT}{first_color}[{tag:^16}]' +
           f'{second_color}{text}{Style.RESET_ALL}'.rjust(60))
