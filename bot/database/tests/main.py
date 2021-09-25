eb75cc05d6b5adc40ac466b4e3673fb5fec46348
Rafael <rafael271alves@gmail.com>
2021-09-25 16:16:12 -0300

    pequenas alterações nas mensagens de log

--- bot/database/tests/main.py
+++ bot/database/tests/main.py
@@ -1,6 +1,8 @@
 from inspect import iscoroutinefunction
 from traceback import format_exc
 
+from colorama import Fore
+
 from database.tests import Configs
 from database.tests.guild_repository import GuildRepositoryTest
 from utils import log
@@ -11,6 +13,8 @@ async def make_tests():
     tests_class = [GuildRepositoryTest]
     log('TESTING', 'STARTING DATABASE TESTS')
     for test_class in tests_class:
+        table_name = test_class.__name__.removesuffix('RepositoryTest')
+        log('TESTING', f'TESTS FOR MODEL "{table_name}"', Fore.CYAN, Fore.MAGENTA)
         tests = sorted(func for func in dir(test_class)
                        if func.startswith('test_'))
         test_obj = test_class()
@@ -21,9 +25,9 @@ async def make_tests():
                     await method()
                 else:
                     method()
-                log('TESTING', f'{test} TEST DONE')
+                log('TESTING', f'{test} SUCCESS')
             except:
-                log('TESTING', f'{test} TEST ERROR')
+                log('TESTING', f'{test} ERROR')
                 print(format_exc())
     await Configs.teardown()
     log('TESTING', 'DATABASE TESTS SUCCESSFULLY')
