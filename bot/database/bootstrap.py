6c3fe10496133e26045a1142b5c247e9c8f3fae7
Rafael <rafael271alves@gmail.com>
2021-09-25 14:56:34 -0300

    logs do bootstrap do db alterados

--- bot/database/bootstrap.py
+++ bot/database/bootstrap.py
@@ -1,8 +1,9 @@
+from colorama import Fore
 from sqlalchemy import inspect
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
-from utils import log
 
 from database.models import __models__
+from utils import log
 
 
 async def check_tables(engine: AsyncEngine):
@@ -13,18 +14,16 @@ async def check_tables(engine: AsyncEngine):
         engine (sqlalchemy.ext.asyncio.engine.AsyncEngine): The database engine.
 
     """
-    log('BOOTSTRAPING DB', 'CHECKING TABLES')
+    log('BOOTSTRAPPING DB', 'CHECKING TABLES', Fore.CYAN, Fore.LIGHTYELLOW_EX)
     async with engine.connect() as conn:
         for model in __models__:
             table_name = model.__tablename__
             if not await conn.run_sync(
-                lambda con: inspect(con).has_table(table_name)
+                    lambda con: inspect(con).has_table(table_name)
             ):
-                log(
-                    'BOOTSTRAPING DB',
-                    f'CREATING TABLE {table_name}'
-                )
+                log('BOOTSTRAPPING DB', f'CREATING TABLE "{table_name}"', Fore.CYAN, Fore.LIGHTYELLOW_EX)
                 await model.create_table(engine)
+    log('BOOTSTRAPPING DB', 'TABLES CHECKING COMPLETED', Fore.CYAN, Fore.LIGHTYELLOW_EX)
 
 
 async def bootstrap(engine: AsyncEngine):
@@ -35,8 +34,6 @@ async def bootstrap(engine: AsyncEngine):
         engine (sqlalchemy.ext.asyncio.engine.AsyncEngine): The database engine.
 
     """
+    log('BOOTSTRAPPING DB', 'STARTING', Fore.CYAN, Fore.WHITE)
     await check_tables(engine)
-    log(
-        'BOOTSTRAPING DB',
-        'DONE'
-    )
+    log('BOOTSTRAPPING DB', 'DONE', Fore.CYAN, Fore.WHITE)
