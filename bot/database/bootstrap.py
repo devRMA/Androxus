055934872b112b5af62dcf5230a774546b64780e
Rafael <rafael271alves@gmail.com>
2021-09-25 00:19:07 -0300

    documentação do bootstrap do banco adicionado

--- bot/database/bootstrap.py
+++ bot/database/bootstrap.py
@@ -1,11 +1,18 @@
-from colorama import Fore
 from sqlalchemy import inspect
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from utils import log
 
 from database.models import __models__
 
 
-async def check_tables(engine):
+async def check_tables(engine: AsyncEngine):
+    """
+    Check if tables exist, if not create them
+
+    Args:
+        engine (sqlalchemy.ext.asyncio.engine.AsyncEngine): The database engine.
+
+    """
     log('BOOTSTRAPING DB', 'CHECKING TABLES')
     async with engine.connect() as conn:
         for model in __models__:
@@ -20,7 +27,14 @@ async def check_tables(engine):
                 await model.create_table(engine)
 
 
-async def bootstrap(engine):
+async def bootstrap(engine: AsyncEngine):
+    """
+    Bootstrap the database.
+
+    Args:
+        engine (sqlalchemy.ext.asyncio.engine.AsyncEngine): The database engine.
+
+    """
     await check_tables(engine)
     log(
         'BOOTSTRAPING DB',
