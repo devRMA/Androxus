b1b6447518f37642a63a341c0519a11fdbd1c127
Rafael <rafael271alves@gmail.com>
2021-09-25 14:49:41 -0300

    adição das permissões necessárias e docstring

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,16 @@
 from os import getenv
 
+from discord import Permissions
+
 
 class Config:
+    """
+    This class is used to store the configuration of the bot.
+
+    Attributes:
+        DEFAULT_PREFIX (str): The default prefix for the bot.
+        REQUIRED_PERMISSIONS (discord.Permissions): The permissions necessary for all bot commands to work.
+
+    """
     DEFAULT_PREFIX = getenv('DEFAULT_PREFIX', '!')
+    REQUIRED_PERMISSIONS = Permissions(8)
