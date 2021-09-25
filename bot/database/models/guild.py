43569fa262591452079441088b56866c562880d2
Rafael <rafael271alves@gmail.com>
2021-09-25 14:57:08 -0300

    adição do método drop_table no model guild

--- bot/database/models/guild.py
+++ bot/database/models/guild.py
@@ -1,7 +1,8 @@
-from config import Config
 from sqlalchemy import BigInteger, Column, String
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
 from sqlalchemy.ext.declarative import declarative_base
+
+from config import Config
 
 Base = declarative_base()
 
@@ -10,6 +11,9 @@ class Guild(Base):
     """
     Represents a guild in the database.
 
+    Args:
+        id (int): The guild id
+        prefix (str, optional): The guild prefix.
 
     Attributes:
         id (int): The guild's ID.
@@ -29,6 +33,9 @@ class Guild(Base):
         self.id = id
         self.prefix = prefix or Config.DEFAULT_PREFIX
 
+    def __eq__(self, other):
+        return self.id == other.id
+
     @staticmethod
     async def create_table(engine: AsyncEngine):
         """
@@ -40,3 +47,15 @@ class Guild(Base):
         """
         async with engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
+
+    @staticmethod
+    async def drop_table(engine: AsyncEngine):
+        """
+        Drops the "guilds" table.
+
+        Args:
+            engine (sqlalchemy.ext.asyncio.engine.AsyncEngine): The database engine.
+
+        """
+        async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
