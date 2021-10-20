14b796b8c8665ab9f1c44a3c144986d9c4a0ce92
Rafael <rafael271alves@gmail.com>
2021-10-19 22:37:24 -0300

    adição de métodos genéricos na classe de model base

--- bot/database/models/model.py
+++ bot/database/models/model.py
@@ -24,3 +24,49 @@ class Model:
     """
     The base model class.
     """
+
+    def diff_in_dict(self, other) -> dict:
+        """
+        Returns a dictionary representation of the difference between the
+        model and another model.
+
+        Args:
+            other (database.models.Model): The model to compare to.
+
+        Returns:
+            dict: The model's dictionary representation.
+
+        """
+        # check if it has the same id and is of the same class
+        if self != other:
+            return {}
+        other_dict = other.to_dict()
+        self_dict = self.to_dict()
+        diff_dict = {}
+        for key in other_dict.keys():
+            if self_dict[key] != other_dict[key]:
+                diff_dict[key] = other_dict[key]
+        return diff_dict
+
+    def fill(self, params: dict):
+        """
+        Update the model with the given parameters.
+
+        Args:
+            params (dict): The parameters to update the model with.
+
+        """
+        for key, value in params.items():
+            if key == 'id':
+                continue
+            setattr(self, key, value)
+
+    def merge(self, other):
+        """
+        Merge the model with another model.
+
+        Args:
+            other (database.models.Model): The model to merge with.
+
+        """
+        self.fill(self.diff_in_dict(other))
