f57228f5f5b0cd4606c0ed1cb4af13feb10c6277
Rafael <rafael271alves@gmail.com>
2022-01-26 21:12:39 -0300

    tipagem adicionada

--- bot/androxus/bot.py
+++ bot/androxus/bot.py
@@ -22,21 +22,22 @@
 
 from datetime import datetime
 from itertools import cycle
-from os import getenv, listdir
+from os import listdir
 from os.path import abspath
-from typing import List
+from platform import python_version
+from typing import Any, Dict, List
 
 from aiohttp.client import ClientSession
 from configs import Configs
 from database import bootstrap as db_bootstrap
 from database.connection import ConnectionFactory
-from disnake import Game, Intents
+from disnake import Game, Intents, Message
 from disnake import __version__ as disnake_version
 from disnake.ext import commands, tasks
 from disnake.utils import utcnow
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
-from stopwatch import Stopwatch
+from stopwatch import Stopwatch  # type: ignore
 from toml import load
 from utils import SingletonMeta, log
 from utils.colors import LBLUE, LGREEN, LYELLOW
@@ -58,27 +59,28 @@ class Bot(commands.Bot, metaclass=SingletonMeta):
         http_session (ClientSession): The aiohttp session.
 
     """
-    start_date: datetime = None
     maintenance: bool = False
-    db_engine: AsyncEngine = None
-    db_session: AsyncSession = None
-    http_session: ClientSession = None
-    configs: Configs = Configs()
+    start_date: datetime
+    db_engine: AsyncEngine
+    db_session: AsyncSession
+    http_session: ClientSession
+    configs = Configs()
     _status = cycle((
         'Androxus V3',
         '{users} Users!',
         '{servers} Guilds!'
     ))
-    _startup_timer: Stopwatch = None
+    _startup_timer: Stopwatch
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self) -> None:
         self._startup_timer = Stopwatch()
         log('BOT', 'STARTING BOT...', first_color=LYELLOW)
 
-        async def _prefix_or_mention(bot, message):
+        async def _prefix_or_mention(bot: 'Bot', message: Message):
             prefix = await get_prefix(bot, message)
             return commands.when_mentioned_or(prefix)(bot, message)
 
+        kwargs = {}
         kwargs['command_prefix'] = _prefix_or_mention
         kwargs['owner_id'] = self.configs.owner_id
         kwargs['case_insensitive'] = True
@@ -86,7 +88,9 @@ class Bot(commands.Bot, metaclass=SingletonMeta):
         kwargs['strip_after_prefix'] = True
         kwargs['activity'] = Game(name='\N{SLEEPING FACE} Starting ...')
         kwargs['test_guilds'] = self.configs.test_guilds
-        super().__init__(*args, **kwargs)
+
+        super().__init__(**kwargs)  # type: ignore
+
         # Loads all cogs of the bot.
         # bot.remove_command('help')
         for cog in get_cogs():
@@ -96,14 +100,16 @@ class Bot(commands.Bot, metaclass=SingletonMeta):
     def __version__(self) -> str:
         path = f'{abspath("./")}/pyproject.toml'
         with open(path, 'r', encoding='utf-8') as file:
-            return load(file)['tool']['poetry']['version']
+            data: Dict[str, Any] = load(file)
+            return data['tool']['poetry']['version']
 
     async def on_ready(self) -> None:
-        if self.db_session is None or self.db_engine is None:
-            self._startup_timer.stop()
-            self.db_engine = ConnectionFactory.get_engine()
-            self.db_session = ConnectionFactory.get_session(self.db_engine)
-            self.start_date = utcnow()
+        if (not hasattr(self, 'db_session')) or (not hasattr(self, 'db_engine')):
+            self._startup_timer.stop()  # type: ignore
+            setattr(self, 'db_engine', ConnectionFactory.get_engine())
+            setattr(self, 'db_session',
+                    ConnectionFactory.get_session(self.db_engine))
+            setattr(self, 'start_date', utcnow())
             await db_bootstrap(self.db_engine)
             log('BOT', f'LOGGED IN {self.user}', first_color=LGREEN)
             log('BOT', f'ID: {self.user.id}', first_color=LGREEN)
@@ -113,11 +119,11 @@ class Bot(commands.Bot, metaclass=SingletonMeta):
                 first_color=LBLUE)
             log('INFO', f'BOT VERSION: {self.__version__}',
                 first_color=LBLUE)
-            log('INFO', 'PYTHON VERSION: ' + getenv('PYTHON_VERSION'),
+            log('INFO', f'PYTHON VERSION: {python_version()}',
                 first_color=LBLUE)
             log('INFO', f'DISNAKE VERSION: {disnake_version}',
                 first_color=LBLUE)
-            log('INFO', f'TIME TO START: {self._startup_timer}',
+            log('INFO', f'TIME TO START: {self._startup_timer}',  # type: ignore
                 first_color=LBLUE)
             try:
                 # starts loop to change status
@@ -134,7 +140,7 @@ class Bot(commands.Bot, metaclass=SingletonMeta):
         await self.change_presence(activity=Game(name=status_name))
 
     def get_languages(self) -> List[str]:
-        languages = []
+        languages: List[str] = []
         languages.append(self.configs.default_language)
         for language in listdir(abspath('./') + '/language/json/'):
             if language.endswith('.json'):
